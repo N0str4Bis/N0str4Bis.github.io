d047ca1467ddd96f75edbe9f53009cd99da9e17b
N0str4 <aymerickvalois@yahoo.fr>
2023-11-08 22:22:09 +0100

    tt

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://http.aymerickvalois.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDg2MGZhMmQ3MDYwMDI1MmY2ZGM1YzM5NjYzNDk4YiIsInN1YiI6IjY1NGJmNzY5NjdiNjEzMDBjODQ5Y2M2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-fr33lPEG_GFcNizHXlxVaRi67bP4gqoBh_5A6Y2IPI"
+};
